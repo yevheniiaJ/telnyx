59bcfa22da47f02b253f9303a1bd2207cb75ed46
yevheniiaJ <testert573@gmail.com>
2025-12-09 00:42:20 +0200

    docker

--- wdio.conf.js
+++ wdio.conf.js
@@ -64,7 +64,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
+          }
     }],
 
     //
