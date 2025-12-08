7b8b9e699363f576c32db7a5de25991bbaada14c
yevheniiaJ <testert573@gmail.com>
2025-12-08 23:01:47 +0200

    test update

--- wdio.conf.js
+++ wdio.conf.js
@@ -146,7 +146,8 @@ export const config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000,
+        require: ['expect-webdriverio']
     },
 
     //
